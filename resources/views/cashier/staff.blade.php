f248a47c63157fb28bd15b754247ce0d2587fe9e
Your Name <jonathandreo@thelewiscollege.edu.ph>
2025-05-05 08:06:15 +0800

    ok

--- resources/views/cashier/staff.blade.php
+++ resources/views/cashier/staff.blade.php
@@ -157,7 +157,7 @@
                     <i class="bi bi-x-lg me-1"></i> Cancel
                 </button>
                 <button type="button" id="confirmReadyBtn" class="btn btn-primary">
-                    <i class="bi bi-check-lg me-1"></i> Confirm Ready
+                    <i class="bi bi-check-lg me-1"></i> Confirm Readyyyyyyyyyyyyyyyyyyy
                 </button>
             </div>
         </div>
