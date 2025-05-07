11770725f6196e2f61264cccd220085310b8effa
Your Name <jonathandreo@thelewiscollege.edu.ph>
2025-05-07 09:36:40 +0800

    yaa namn

--- resources/views/items/index.blade.php
+++ resources/views/items/index.blade.php
@@ -160,7 +160,7 @@ function checkTimeAndResetFood() {
     const forceReset = true;
 
     // Check if it's 4:00 PM (16:00) or if we're forcing reset for testing
-    if ((hours === 16 && minutes === 0) || forceReset) {
+    if ((hours === 24 && minutes === 0) || forceReset) {
         console.log('Condition met! Attempting to reset food quantities...');
         
         // Send AJAX request to reset food quantities
